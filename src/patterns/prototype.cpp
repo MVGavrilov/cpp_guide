c9a9e0b36a6d1f9488918670f78c7fc2f1063c51
Mikhail Gavrilov <mikhail.gavrilov@skoltech.ru>
2022-06-03 02:34:04 +0600

    Add prototype

---
+++ src/patterns/prototype.cpp
@@ -0,0 +1,59 @@
+// Подробнее в https://refactoring.guru/ru/design-patterns/prototype
+
+#include <string>
+#include <iostream>
+
+class Robot {
+protected:
+    std::string name;
+public:
+    Robot(std::string name) : name(name) {}
+    virtual ~Robot() = default;
+    virtual Robot* clone() = 0;
+    virtual std::string voice() = 0;
+};
+
+class RoboDog : public Robot {
+public:
+    RoboDog(const std::string& name) : Robot(name) {} //Пример делегирующего конструктора
+    Robot* clone() override {
+        return new RoboDog(*this);
+    }
+    std::string voice() override {
+        return name + " says 'Woof'";
+    }
+};
+
+class RoboCat : public Robot {
+public:
+    RoboCat(const std::string& name) : Robot(name) {}
+    Robot* clone() override {
+        return new RoboCat(*this);
+    }
+    std::string voice() override {
+        return name + " says 'Meow'";
+    }
+};
+
+int main() {
+    Robot* animal1 = new RoboDog("Sharik");
+    Robot* animal2 = new RoboCat("Murka");
+    /*
+     * Мы не знаем, какой тип животного реализует интерфейс Robot, но функция clone() позволяет
+     * нам об этом не задумываться
+     */
+    Robot* animal1_copy = animal1->clone();
+    Robot* animal2_copy = animal2->clone();
+
+    std::cout << animal1->voice() << std::endl;
+    std::cout << animal2->voice() << std::endl;
+    std::cout << animal1_copy->voice() << std::endl;
+    std::cout << animal2_copy->voice() << std::endl;
+
+    delete animal1;
+    delete animal2;
+    delete animal1_copy;
+    delete animal2_copy;
+    return 0;
+}
+
